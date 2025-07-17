2b32477b6ba52176a42d8656d6ffa50f355f3b5d
Carmen Burgarelli <cb5120@students.necsd.net>
2025-07-17 15:23:37 -0400

    project two completed i think

--- server/appServer.js
+++ server/appServer.js
@@ -1,13 +1,36 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const feedController = require('./controller/feedController');
+
 const app = express();
-app.use(express.static('client/public'));
+const PORT = 1337;
 
-app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: './client/views'})
-})
+// Serve static files
+app.use(express.static(path.join(__dirname, 'client/public')));
 
-app.get('/feed', function(req, res) {
-    res.sendFile('feed.html', {root: './client/views'})
-})
+// Parse JSON bodies
+app.use(bodyParser.json());
 
-app.listen(1337, () => console.log('Listening on port 1337.'))
+// Serve HTML views
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'client/views') });
+});
+
+app.get('/feed', (req, res) => {
+    res.sendFile('feed.html', { root: path.join(__dirname, 'client/views') });
+});
+
+// API Routes for FeedItem
+app.route('/api/FeedItem')
+    .get(feedController.getAllFeedItem)
+    .post(feedController.saveFeedItemHandler);
+
+app.route('/api/FeedItem/:ItemId')
+    .get(feedController.getFeedItem)
+    .delete(feedController.deleteFeedItem)
+    .patch(feedController.updateFeedItem);
+
+// Start the server
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+
